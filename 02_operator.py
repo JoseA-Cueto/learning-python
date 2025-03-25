8353d9af566df885b5b9fdfe35180e77ea91a1ff
Jose <joseacm2901@gmail.com>
2025-03-25 12:59:52 -0400

    Operadores con string

--- 02_operator.py
+++ 02_operator.py
@@ -6,3 +6,9 @@ print(3/4)#Divide
 print(15%2)#Da el Resto
 print(10//6)#Division que da un resultado aproximado a un numero entero(Division de suelo)
 print(2**4)#Exponente en este caso 2 a la 4
+
+print("Hola "  + "Pyton") #Tambien podemos operar con string (esto se llama concatenacion)
+
+print("Hola "  + str(5))### Si bien no es posible hacer sumas entre distintos tipos de datos 
+                        ### podemos usar str(convierte a string)
+print("Hola " * 5) #Multiplicaciones si s pueden hacer 
