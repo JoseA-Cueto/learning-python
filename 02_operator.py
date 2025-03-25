8850938cf3ec17b4cc1094605f288c8b571b6161
Jose <joseacm2901@gmail.com>
2025-03-25 12:44:16 -0400

    Oerator(Division de suelo,Exponente)

--- 02_operator.py
+++ 02_operator.py
@@ -4,3 +4,5 @@ print(3-4)#Resta
 print(3*4)#Multiplica
 print(3/4)#Divide
 print(15%2)#Da el Resto
+print(10//6)#Division que da un resultado aproximado a un numero entero(Division de suelo)
+print(2**4)#Exponente en este caso 2 a la 4
