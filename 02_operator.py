d63f865d515be804b5a4719fb16c409c5acc6bac
Jose <joseacm2901@gmail.com>
2025-03-25 13:13:19 -0400

    Operadores Comparativos

--- 02_operator.py
+++ 02_operator.py
@@ -12,3 +12,20 @@ print("Hola "  + "Pyton") #Tambien podemos operar con string (esto se llama conc
 print("Hola "  + str(5))### Si bien no es posible hacer sumas entre distintos tipos de datos 
                         ### podemos usar str(convierte a string)
 print("Hola " * 5) #Multiplicaciones si s pueden hacer 
+
+
+
+
+#Operadores Comparativos
+print(3>4)
+print(3<4)
+print(3>=4)
+print(4<=4)
+print(3 == 4)
+print(3!=4)
+
+
+#Tambien podemos hacer comparaciones con los string ejmeplo:
+print("Hola" < "Python")
+print("Hola" == "Python")
+print("Hola"<"Zola")#Comparacion alfabetica en un orden
