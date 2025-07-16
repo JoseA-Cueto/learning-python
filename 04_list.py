1dc3a43ac154a4d85eae6eee252aca8bb9dffcdd
Jose Antonio Cueto Mengana <joseantonio.cueto@ingecid.es>
2025-07-16 12:33:12 +0200

    minor change list

--- 04_list.py
+++ 04_list.py
@@ -9,3 +9,10 @@ my_list = [10,12,123,123,1,4,46,756,234]
 
 print (len(my_list))
 print (my_list)
+my_otherlist = [35,1.87,"Jose","Cueto"]
+print(my_otherlist)
+
+print(my_otherlist[0])
+
+
+print(type(my_list))
